acb24db7e4fa6857d9c3a9d380aca58e75809a1d
Vika Polshkova <vikagornushkina@yandex.ru>
2025-04-01 23:17:30 +0500

    release 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function createCard(element, removeCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  removeCard(cardDeleteButton);
+  return cardElement;
+}
 
-// @todo: Функция удаления карточки
+function removeCard(button) {
+  button.addEventListener('click', function () {
+    const card = button.closest('.places__item');
+    if (card) {
+      card.remove();
+    }
+  });
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(element) {
+  placesList.append(createCard(element, removeCard));
+});
