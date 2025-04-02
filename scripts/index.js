e2fce56f43ab3185f2961ae1bedbc15f7d85d7ff
Vika Polshkova <vikagornushkina@yandex.ru>
2025-04-03 00:12:01 +0500

    release 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,20 @@ const placesList = document.querySelector('.places__list');
 function createCard(element, removeCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   cardElement.querySelector('.card__title').textContent = element.name;
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelector('.card__image').alt = element.name;
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-  removeCard(cardDeleteButton);
+  cardDeleteButton.addEventListener('click', function () {
+    removeCard(cardElement);
+  });
   return cardElement;
 }
 
-function removeCard(button) {
-  button.addEventListener('click', function () {
-    const card = button.closest('.places__item');
+function removeCard(card) {
     if (card) {
       card.remove();
     }
-  });
 }
 
 initialCards.forEach(function(element) {
